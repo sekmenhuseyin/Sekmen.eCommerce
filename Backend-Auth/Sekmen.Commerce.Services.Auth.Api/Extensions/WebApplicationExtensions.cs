bcc84c27f392e5c532ebd155d0584fabe0ff302c
Hüseyin Sekmenoğlu <huseyin@sekmen.dev>
2024-10-15 14:52:23 +0300

    fix: updated auth api project

---
+++ Backend-Auth/Sekmen.Commerce.Services.Auth.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,24 @@
+namespace Sekmen.Commerce.Services.Auth.Api.Extensions;
+
+public static class WebApplicationExtensions
+{
+    internal static void AddInternalDependencies(this WebApplicationBuilder builder)
+    {
+        _ = builder.Services
+            //.AddAutoMapper(typeof(ICommand))
+            //.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<ICommand>())
+            .AddDbContext<AuthDbContext>(options => 
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            );
+    }
+
+    internal static void ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AuthDbContext>();
+        if (db.Database.GetPendingMigrations().Any())
+        {
+            db.Database.Migrate();
+        }
+    }
+}
---
+++ Backend-Auth/Sekmen.Commerce.Services.Auth.Api/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using Microsoft.EntityFrameworkCore;
+global using Sekmen.Commerce.Services.Auth.Api.Extensions;
+global using Sekmen.Commerce.Services.Auth.Infrastructure;
